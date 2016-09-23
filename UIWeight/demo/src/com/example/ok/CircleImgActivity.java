904e9b9899551d053cbc5133f2d79f98f90a076b
pengju1218 <920988751@qq.com>
2016-09-23 15:08:32 +0800

    Signed-off-by: pengju1218 <920988751@qq.com>

--- UIWeight/demo/src/com/example/ok/CircleImgActivity.java
+++ UIWeight/demo/src/com/example/ok/CircleImgActivity.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.csdn.Weight.ImageView.RoundImageView;
 import com.csdn.Weight.ImageView.displayers.CircleBitmapDisplayer;
 import com.example.ok.Base.BaseActivity;
+import com.example.ok.util.OrderUtil;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
@@ -14,16 +16,22 @@ public class CircleImgActivity extends BaseActivity implements View.OnClickListe
     private android.widget.ImageView circleimg;
 
     String imageUrl = "http://d.hiphotos.baidu.com/image/pic/item/9358d109b3de9c8242a7de176e81800a18d84363.jpg";
+    private com.csdn.Weight.ImageView.RoundImageView userAvater;
+
+    private ImageLoader imageLoader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.circle_img);
+        this.userAvater = (RoundImageView) findViewById(R.id.userAvater);
+        imageLoader = OrderUtil.getInstance().initImageLoader(CircleImgActivity.this, imageLoader, "test");
         initImageLoader();
         //ImageLoader.getInstance().init(ImageLoaderConfiguration.createDefault(CircleImgActivity.this));
         this.circleimg = (ImageView) findViewById(R.id.circle_img);
 
-        ImageLoader.getInstance().displayImage(imageUrl,circleimg,options);
+        ImageLoader.getInstance().displayImage(imageUrl, circleimg, options);
+        imageLoader.displayImage(imageUrl,userAvater);
 
     }
 
@@ -33,15 +41,16 @@ public class CircleImgActivity extends BaseActivity implements View.OnClickListe
             .cacheOnDisk(true)
             .displayer(new CircleBitmapDisplayer())
             .build();
+
     @Override
     public void onClick(View v) {
 
     }
 
-    private void initImageLoader(){
+    private void initImageLoader() {
         ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext())
                 .denyCacheImageMultipleSizesInMemory()
-                .diskCacheExtraOptions(1024,1024,null)
+                .diskCacheExtraOptions(1024, 1024, null)
                 .memoryCacheSize(2 * 1024 * 1024)
                 .discCacheSize(50 * 1024 * 1024)
                 .discCacheFileCount(100)
